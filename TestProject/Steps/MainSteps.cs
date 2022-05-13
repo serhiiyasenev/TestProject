b82944810c7798dfdb38c67004b576e74136d60f
Serhii Yasenev <serhii.yasenev@ihsmarkit.com>
2022-05-13 13:06:03 +0300

    del comment

--- TestProject/Steps/MainSteps.cs
+++ TestProject/Steps/MainSteps.cs
@@ -5,7 +5,6 @@ namespace TestProject.Steps
 {
     public class MainSteps : BaseSteps
     {
-        //test
         MainPage MainPage => new();
 
         public void SetValueForCalculation(string value)
